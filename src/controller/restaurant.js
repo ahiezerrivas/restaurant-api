a0b9652d0f05cc06eb33dcc1ef36da0529f735b1
Ahiezer Rivas <ahiezerrivas@gmail.com>
2019-02-26 14:27:23 -0300

    Retrieving Data (GET requests in Node)

--- src/controller/restaurant.js
+++ src/controller/restaurant.js
@@ -4,6 +4,7 @@ import Restaurant from '../model/restaurant';
 
 export default ({ config, db}) => {
     let api = Router();
+    // CRUD - Create Read Update Delete
 
     // '/v1/restaurant/add
     api.post('/add', (req, res) => {
@@ -15,6 +16,26 @@ export default ({ config, db}) => {
                 res.send(err);
             }
             res.json({ message: 'Restaurant saved succesfully'});
+        });
+    });
+
+    // '/v1/restaurant' - Read
+    api.get('/', (req, res) => {
+        Restaurant.find({}, (err, restaurants) => {
+            if (err) {
+                res.send(err);
+            }
+            res.json(restaurants);
+        });
+    });
+
+    // '/v1/restaurant/:id' - Read 1
+    api.get('/:id', (req, res) => {
+        Restaurant.findById(req.params.id, (err, restaurant) => {
+            if (err) {
+                res.send(err);
+            }
+            res.json(restaurant);
         });
     });
 
